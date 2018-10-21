36600eb812d71755b3c4d2f76d1c0ad51366870a
Ipek Inan <kepinani1@gmail.com>
2018-10-21 12:28:56 -0400

    newest version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,239 +1,235 @@
-//Game is set at 60 seconds, counting down by 1. Upon calling this function, the start page will be hidden from view and the second page with trivia questions should appear.
-var gameSet = {
-    timeRemaining: 60,
-    startTimer: function() {
-      $("#timer").text("Time remaining: " + gameSet.timeRemaining);
-      setInterval(gameSet.countdown, 1000);
-      $(".start-page").hide();
-      $(".container").show(triviaGame.showQuestions);
-    },
-    //As the timer counts down, and becomes 0, the timer will stop.
-    countdown: function() {
-      gameSet.timeRemaining--;
-      $("#timer").text("Time remaining: " + gameSet.timeRemaining);
-      if (gameSet.timeRemaining === 0) {
-        gameSet.stopTimer();
-        $("#timer").empty();
-      }
-    },
-    //Once timer stops, the interval will be cleared and answer checker function will start.
-    stopTimer: function() {
-      clearInterval();
-      triviaGame.answerChecker();
-    },
-    //Final page will appear showing the total right, wrong, and answers left blank.
-    showFinalPage: function(numCorrect, numIncorrect, numUnanswered) {
-      $("#finalPage").show();
-      $("#questionsArea").empty();
-      $("#timer").empty();
-      $("#timer").hide();
-      $("#rightAnswers").text("Correct answers: " + numCorrect);
-      $("#wrongAnswers").text("Incorrect answers: " + numIncorrect);
-      $("#leftBlank").text("Left blank: " + numUnanswered);
-    }
-  };
-  //variable triviaGame is defined here in which upon being activated, a header, form with questions, and answers will appear. Anwer choices will have radio capability, and checker user input storage.Questions will be taken from an array, and user answers will be appended.
-  var triviaGame = {
-    showQuestions: function() {
-      var divContainer = $(".container", "#questionsArea","#form");
-      divContainer.append(
-        "<h2>Take the challenge, and answer the following questions:</h2>"
-      );
-      //for loop takes questions from the gameQuestions array and displays them on the screen.
-      for (var i = 0; i < gameQuestions.length; i++) {
-        divContainer.append(
-          '<div id="questionsArea">' + gameQuestions[i].question + "</div>"
-        );
-  
-        var answer1 = gameQuestions[i].answers[0];
-        var answer2 = gameQuestions[i].answers[1];
-        var answer3 = gameQuestions[i].answers[2];
-        var answer4 = gameQuestions[i].answers[3];
-  
-        divContainer.append(
-          '<div id="form" type="radio" name="radio-group' +
-            i +
-            '" id="radio' +
-            i +
-            '"><label class="form-label" id="radio' +
-            i +
-            'label" for="radio' +
-            i +
-            '">' +
-            answer1 +
-            "</label></div>"
-        );
-        divContainer.append(
-          '<div id="form" type="radio" name="radio-group' +
-            i +
-            '" id="radio' +
-            i +
-            '"><label class="form-label" id="radio' +
-            i +
-            'label" for="radio' +
-            i +
-            '">' +
-            answer2 +
-            "</label></div>"
-        );
-        divContainer.append(
-          '<div id="form" type="radio" name="radio-group' +
-            i +
-            '" id="radio' +
-            i +
-            '"><label class="form-label" id="radio' +
-            i +
-            'label" for="radio' +
-            i +
-            '">' +
-            answer3 +
-            "</label></div>"
-        );
-        divContainer.append(
-          '<div id="form" type="radio" name="radio-group' +
-            i +
-            '" id="radio' +
-            i +
-            '"><label class="form-label" id="radio' +
-            i +
-            'label" for="radio' +
-            i +
-            '">' +
-            answer4 +
-            "</label></div>"
-        );
-      }
-      //A done button will appear in which user can submit answers at the very end and the timer stops.
-      var doneButton =
-        '<button id="btn btn-primary" id="done-button" type="submit">Done</button>';
-      divContainer.append(doneButton);
-      $("#done-button").on("click", gameSet.stopTimer);
-    },
-//Answer checker will compile the number of correct, incorrect, and unanswered according to what the user inputted (checked off) versus what was the correct answer according to the array gameQuestions.
-    answerChecker: function() {
-      var rightAnswer;
-      var userAnswer;
-      var numCorrect = 0;
-      var numIncorrect = 0;
-      var numUnanswered = 0;
-  
-      for (var i = 0; i < gameQuestions.length; i++) {
-        rightAnswer = gameQuestions[i].correct;
-        userAnswer = $("input[id=radio" + i + "").on("click", checked.label);
-  
-        if (userAnswer === rightAnswer) {
-          numCorrect++;
-        } else if (userAnswer === "") {
-          numUnanswered++;
-        } else if (userAnswer !== rightAnswer) {
-          {
-            numIncorrect++;
-          }
-        }
-      }
-      gameSet.showFinalPage(numCorrect, numIncorrect, numUnanswered);
-    }
-  };
-  var gameQuestions = [
-    {
-      question:
-        "What worldwide hit by Nelly Furtado was #1 on over 20 different music charts?",
-      answers: [
-        "'Say It Right,' with that Timbaland dude",
-        "'Promiscuous,' duh",
-        "Genie in a bottle,' that's Nelly, right?",
-        "'I'm Like a Bird'"
-      ],
-      correct: "Say It Right"
-    },
-    {
-      question:
-        "What famous rapper said 'I'll take you to the candy shop..I'll let you lick the lollipop'",
-      answers: [
-        "Wait, who?",
-        "50 cent",
-        "Nelly probably did, I don't remember",
-        "Beyonce rapped back then, definately"
-      ],
-      correct: "50 cent"
-    },
-    {
-      question: "What band is not from the 2000s?",
-      answers: [
-        "Fall Out Boy",
-        "Paramore",
-        "Guns and Roses, they were still rockin it, right?",
-        "The Killers, I think"
-      ],
-      correct: "Guns and Roses, they were still rocking it, right?"
-    },
-    {
-      question: "Right Thurr-- is a hit single for what artist?",
-      answers: ["Chingy", 
-      "Kanye West", 
-      "Ludacris", 
-      "TI, he was around then...."],
-      correct: "Chingy"
-    },
-    {
-      question: "Which is not a top 2000s pop hit?",
-      answers: [
-        "'Gold Digger' by Kanye West",
-        "'We Belong Together' by Mariah Carey",
-        "'Boom Boom Pow' by the Black Eyed Peas",
-        "'Trap Queen' by Fetty Wap"
-      ],
-      correct: "'Trap Queen' by Fetty Wap"
-    },
-    {
-      question: "Complete the lyrics 'Shake it like a __'",
-      answers: [
-        "Polaroid picture",
-        "Salt shacker",
-        "Rattle snake",
-        "Drunk in love"
-      ],
-      correct: "Polaroid picture"
-    },
-    {
-      question: "What famous couple split in 2002?",
-      answers: ["Britney Spears and Justin Timberlake",
-      "Nick Lachey and Jessica Simpson",
-      "Jennifer Lopez and Puff Daddy",
-      "Who cares? You think I followed celeb couples back then."],
-      correct: "Britney Spears and Justin Timberlake"
-    },
-    {
-      question: "Who was featured on Jojo's 'Baby It's You?'",
-      answers: ["Cee lo Green", 
-      "Eminem", 
-      "Bow wow", 
-      "Nick Cannon"],
-      correct: "Bow Wow"
-    },
-    {
-      question: "Who sang the hits 'Foolish' and 'Happy'?",
-      answers: ["Aaliyah", 
-      "Ashanti", 
-      "Mariah Carey", 
-      "Paula Abdul"],
-      correct: "Ashanti"
-    },
-    {
-      question: "What Eminem album was released in 2002?",
-      answers: [
-        "Encore",
-        "The Marshal Mathers LLP",
-        "'Relapse,' that was an Eminem album, I think",
-        "The Eminem Show"
-      ],
-      correct: "The Marshal Mathers LLP"
-    }
-  ];
-  
-  
-  $(document).ready(function() {
-    $("#startButton").on("click", gameSet.startTimer);
-    divContainer.render();
-    $(".container").render();
+var questions = [
+  {
+    question:
+      "What worldwide hit by Nelly Furtado was #1 on over 20 different music charts?",
+    answers: [
+      { answer: "'Say It Right,' with that Timbaland dude", value: true },
+      { answer: "'Promiscuous,' duh", value: false },
+      { answer: "Genie in a bottle,' that's Nelly, right?", value: false },
+      { answer: "'I'm Like a Bird'", value: false }
+    ]
+  },
+  {
+    question:
+      "What famous rapper said 'I'll take you to the candy shop..I'll let you lick the lollipop'",
+    answers: [
+      { answer: "Wait, who?", value: false },
+      { answer: "50 cent", value: true },
+      { answer: "Nelly probably did, I don't remember", value: false },
+      { answer: "Beyonce rapped back then, definately", value: false }
+    ]
+  },
+  {
+    question: "What band is not from the 2000s?",
+    answers: [
+      { answer: "Fall Out Boy", value: false },
+      { answer: "Paramore", value: false },
+      { answer: "Guns and Roses, they were still rockin it, right?", value: true },
+      { answer: "The Killers, I think", value: false }
+    ]
+  },
+  {
+    question: "Right Thurr-- is a hit single for what artist?",
+    answers: [
+      { answer: "Chingy", value: true },
+      { answer: "Kanye West", value: false },
+      { answer: "Ludacris", value: false },
+      { answer: "TI, he was around then....", value: false }
+    ]
+  },
+  {
+    question: "Which is not a top 2000s hit?",
+    answers: [
+      { answer: "'Gold Digger' by Kanye West", value: false },
+      { answer: "'We Belong Together' by Mariah Carey", value: false },
+      { answer: "'Boom Boom Pow' by the Black Eyed Peas", value: false },
+      { answer: "'Trap Queen' by Fetty Wap", value: true }
+    ]
+  },
+  {
+    question: "Complete the lyrics 'Shake it like a __'",
+    answers: [
+      { answer: "Polaroid picture", value: true },
+      { answer: "Salt shacker", value: false },
+      { answer: "Rattle snake", value: false },
+      { answer: "Drunk in love", value: false }
+    ]
+  },
+  {
+    question: "What famous couple split in 2002?",
+    answers: [
+      { answer: "Britney Spears and Justin Timberlake", value: true },
+      { answer: "Nick Lachey and Jessica Simpson", value: false },
+      { answer:"Jennifer Lopez and Puff Daddy", value: false },
+      { answer: "Who cares? You think I followed celeb couples back then.", value: false }
+    ]
+  },
+  {
+    question: "Who was featured on Jojo's 'Baby It's You?'",
+    answers: [
+      { answer: "Cee lo Green", value: false },
+      { answer: "Eminem", value: false },
+      { answer: "Bow wow", value: true },
+      { answer: "Nick Cannon", value: false }
+    ]
+  },
+  {
+    question: "Who sang the hits 'Foolish' and 'Happy'?",
+    answers: [
+      { answer: "Aaliyah", value: false },
+      { answer: "Ashanti", value: true },
+      { answer: "Mariah Carey", value: false },
+      { answer: "Paula Abdul", value: false }
+    ]
+  },
+  {
+    question: "What Eminem album was released in 2002?",
+    answers: [
+      { answer: "Encore", value: false },
+      { answer: "The Marshal Mathers LLP", value: true },
+      { answer: "'Relapse,' that was an Eminem album, I think", value: false },
+      { answer: "The Eminem Show", value: false }
+    ]
+  },
+];
+// Global variables
+var game;
+var counter = 0;
+var clock;
+var timer = 30;
+var rightCounter = 0;
+var wrongCounter = 0;
+var blankCounter = 0;
+
+$(document).ready(function () {
+  // Start the game when start button is clicked
+  $('.answers').css('visibility', 'hidden');
+  $('body').on('click', '.start-btn', function (event) {
+    event.preventDefault();
+    beginGame();
+    $('.answers').css('visibility', 'visible');
   });
+
+  $('body').on('click', '.answer', function (event) {
+    // console.log($(this));
+    userAnswer = $(this).text();
+    var answerCounter = questions[counter].answers;
+//Looping through the array, the answer whether true or false will be recorded as such and the background of the answer area will change accordingly (i.e., change to green if true===right answer choice). The clock interval will also be cleared upon user click (whether right or wrong answer chosen).
+    var answer = $('.answer');
+    for (var i = 0; i < answerCounter.length; i++) {
+      if (userAnswer === answerCounter[i].answer && answerCounter[i].value === true) {
+        clearInterval(clock);
+        var right = $(this).attr('class', 'right-answer answer');
+        rightAnswer();
+      } else if (userAnswer === answerCounter[i].answer && answerCounter[i].value === false) {
+        clearInterval(clock);
+        $(this).attr('class', 'wrong-answer answer');
+        $('.first-answer').css('background-color', 'green');
+        $('.first-answer').css('color', 'white');
+        wrongAnswer();
+      }
+    }
+  });
+
+  $('body').on('click', '.reset-button', function (event) {
+    event.preventDefault();
+    resetGame();
+  });
+});
+//Counter function to for when right answers chosen. 
+function rightAnswer() {
+  rightCounter++;
+  $('.time').html(timer);
+  $('.right').html('<p>Right answers: ' + rightCounter + '</p><br>');
+  setTimeout(questionCounter, 2000);
+}
+//Counter function to for when wrong answers chosen. 
+function wrongAnswer() {
+  wrongCounter++;
+  $('.time').html(timer);
+  $('.wrong').html('<p>Wrong answers: ' + wrongCounter + '</p>');
+  setTimeout(questionCounter, 2000);
+}
+//function for when answer is left blank by the user and time has expired.
+function unanswered() {
+  blank++;
+  $('.main').append("<p class='times-up'>Time's up!</p>");
+  $('.right-answer').css('background-color', 'green');
+  $('.times-up')
+    .delay(2000)
+    .fadeOut(400);
+  setTimeout(questionCounter, 2000);
+}
+
+// Begin the game. The start page should disappear, the questions area (which includes answer input area), and time should appear.
+function beginGame() {
+  $('.start-page').css('display', 'none');
+  $('.questions-page').css('visibility', 'visible');
+  $('.timer').html('<p>Time remaining: <span class="time">30</span></p>');
+
+  $('.question').html(questions[counter].question);
+  var showingAnswers =
+    '<p class="answer first-answer">' +
+    questions[counter].answers[0].answer +
+    '</p><p class="answer">' +
+    questions[counter].answers[1].answer +
+    '</p><p class="answer">' +
+    questions[counter].answers[2].answer +
+    '</p><p class="answer">' +
+    questions[counter].answers[3].answer +
+    '</p>';
+
+  $('.answers').html(showingAnswers);
+
+  timerHolder();
+}
+
+function questionCounter() {
+  if (counter < 6) {
+    counter++;
+    beginGame();
+    timer = 30;
+    timerHolder();
+  } else {
+    endGame();
+  }
+}
+
+// Timer function
+function timerHolder() {
+  clearInterval(clock);
+  clock = setInterval(seconds, 1000);
+  function seconds() {
+    if (timer === 0) {
+      clearInterval(clock);
+      unanswered();
+    } else if (timer > 0) {
+      timer--;
+    }
+    $('.time').html(timer);
+  }
+}
+
+// Finishing the game. There will be a display of right answers, wrong answers, and blank answers chosen count. These are all inner html items that will be appended to the screen.
+function endGame() {
+  var final = $('.main')
+    .html("<p>Here are your results!<p><br><br>")
+    .append('<p>Correct Answers: ' + rightCounter + '</p><br>')
+    .append('<p>Incorrect Answers: ' + wrongCounter + '</p>');
+  $(final).attr('<div>');
+  $(final).attr('class', 'final');
+  $('.final').append('<p><a class="btn btn-primary btn-lg reset-button" href="#">Restart the game!</a></p>');
+  $('.questions-page').css('display', 'none');
+  $('.answers').css('display', 'none');
+}
+
+// Please note**Reset the game. I added this function to reset the game but when you click the reset button, the game does not reset. Not sure why but I tried a couple of times to fix this.
+function resetGame() {
+  counter = 0;
+  rightCounter = 0;
+  wrongCounter = 0;
+  blankCounter = 0;
+  timer = 30;
+  beginGame();
+  timerHolder();
+}
